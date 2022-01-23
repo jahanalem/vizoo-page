6780196f98797989220d66850a5dd92a27b7acc2
Said Roohullah Allem <s.r.alem19@gmail.com>
2022-01-23 22:00:29 +0100

    removed spec.ts files

--- src/app/partner-section/partner-list/partner-list.component.ts
+++ src/app/partner-section/partner-list/partner-list.component.ts
@@ -13,18 +13,18 @@ export class PartnerListComponent implements OnInit {
 
   selectedPartners: IPartner[];
   partners: IPartner[] = [
-    { id: 1, region: Region.Europe, country: "Germany", name: "Assyst GmbH", imageUrl: "assyst.jpg", websiteUrl: "http://www.human-solutions.com" },
-    { id: 2, region: Region.Asia, country: "Turkey", name: "Astaş Juki", imageUrl: "juki-logo_800x600_fill.jpg", websiteUrl: "https://astasjuki.com" },
-    { id: 3, region: Region.Asia, country: "Japan", name: "Born Digital, Inc.", imageUrl: "BornDigital_120.png", websiteUrl: "https://www.borndigital.co.jp/" },
-    { id: 4, region: Region.America, country: "United States", name: "Browzwear Solutions Pte Ltd.", imageUrl: "BW_120.png", websiteUrl: "https://browzwear.com" },
-    { id: 5, region: Region.Asia, country: "China, Hong Kong and Singapore", name: "Browzwear Solutions Pte Ltd.", imageUrl: "BW_120.png", websiteUrl: "https://browzwear.com" },
-    { id: 6, region: Region.America, country: "United States", name: "Digitize Designs", imageUrl: "DigitizeDesigns_120.png", websiteUrl: "https://digitizedesigns.com" },
-    { id: 7, region: Region.Asia, country: "China", name: "Beijing Leatech Co., Ltd", imageUrl: "Leatech_120.png", websiteUrl: "#" },
-    { id: 8, region: Region.Asia, country: "Turkey", name: "Masterkey Virtual Fashion", imageUrl: "Masterkey_120.png", websiteUrl: "https://www.masterkey.com.tr" },
-    { id: 9, region: Region.Asia, country: "Greece, Turkey", name: "Polytropon", imageUrl: "Polytropon_120.png", websiteUrl: "#" },
-    { id: 10, region: Region.Europe, country: "Italy", name: "Prisma Tech, S.r.l.", imageUrl: "Prisma_120.png", websiteUrl: "https://www.prisma-tech.it" },
-    { id: 11, region: Region.Asia, country: "Pakistan", name: "Triple Tree Solutions", imageUrl: "TripleTreeSolutions__120.png", websiteUrl: "https://www.3-tree.com" },
-    { id: 12, region: Region.Asia, country: "Taiwan", name: "UFA Enterprise Co Ltd", imageUrl: "UFA_120.png", websiteUrl: "https://www.ufado.com.tw" },
+    { id: 1, region: Region.Europe, country: "Germany", name: "Assyst GmbH", imageUrl: "../../../assets/images/assyst.jpg", websiteUrl: "http://www.human-solutions.com" },
+    { id: 2, region: Region.Asia, country: "Turkey", name: "Astaş Juki", imageUrl: "../../../assets/images/juki-logo_800x600_fill.jpg", websiteUrl: "https://astasjuki.com" },
+    { id: 3, region: Region.Asia, country: "Japan", name: "Born Digital, Inc.", imageUrl: "../../../assets/images/BornDigital_120.png", websiteUrl: "https://www.borndigital.co.jp/" },
+    { id: 4, region: Region.America, country: "United States", name: "Browzwear Solutions Pte Ltd.", imageUrl: "../../../assets/images/BW_120.png", websiteUrl: "https://browzwear.com" },
+    { id: 5, region: Region.Asia, country: "China, Hong Kong and Singapore", name: "Browzwear Solutions Pte Ltd.", imageUrl: "../../../assets/images/BW_120.png", websiteUrl: "https://browzwear.com" },
+    { id: 6, region: Region.America, country: "United States", name: "Digitize Designs", imageUrl: "../../../assets/images/DigitizeDesigns_120.png", websiteUrl: "https://digitizedesigns.com" },
+    { id: 7, region: Region.Asia, country: "China", name: "Beijing Leatech Co., Ltd", imageUrl: "../../../assets/images/Leatech_120.png", websiteUrl: "#" },
+    { id: 8, region: Region.Asia, country: "Turkey", name: "Masterkey Virtual Fashion", imageUrl: "../../../assets/images/Masterkey_120.png", websiteUrl: "https://www.masterkey.com.tr" },
+    { id: 9, region: Region.Asia, country: "Greece, Turkey", name: "Polytropon", imageUrl: "../../../assets/images/Polytropon_120.png", websiteUrl: "#" },
+    { id: 10, region: Region.Europe, country: "Italy", name: "Prisma Tech, S.r.l.", imageUrl: "../../../assets/images/Prisma_120.png", websiteUrl: "https://www.prisma-tech.it" },
+    { id: 11, region: Region.Asia, country: "Pakistan", name: "Triple Tree Solutions", imageUrl: "../../../assets/images/TripleTreeSolutions__120.png", websiteUrl: "https://www.3-tree.com" },
+    { id: 12, region: Region.Asia, country: "Taiwan", name: "UFA Enterprise Co Ltd", imageUrl: "../../../assets/images/UFA_120.png", websiteUrl: "https://www.ufado.com.tw" },
   ]
 
   constructor() {
